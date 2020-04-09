c8b3bd59047cf311c90f53c430e8d4d3d42a7f43
reyanshgupta <56816381+reyanshgupta@users.noreply.github.com>
2020-04-10 00:42:05 +0530

    Add files via upload

---
+++ q11.py
@@ -0,0 +1,4 @@
+time1=int(input("Enter the initial time: "))
+time2=int(input("Enter the final time: "))
+diff = time2-time1
+print("Difference in time: ", diff)
---
+++ q9.py
@@ -0,0 +1,3 @@
+miles=float(input("Enter the distance in miles: "))
+km= miles*1.609
+print("No. Of Kilometers: ", km)
