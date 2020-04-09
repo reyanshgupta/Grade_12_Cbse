0d0bb780b9bfe4882b5ac6ab3b4cc70bccfe2a15
reyanshgupta <56816381+reyanshgupta@users.noreply.github.com>
2020-04-09 19:07:09 +0530

    Add files via upload

---
+++ date.py
@@ -0,0 +1,5 @@
+
+x=input("Enter Date in MMDDYYYY format :")
+ardb = ["E","January", "February", "March", "April", "May", "June", "JUly", "August","September", "October", "November", "December" ]
+print(x[:2], x[2:4], x[4:])
+print(ardb[int(x[:2])], x[2:4], x[4:])
